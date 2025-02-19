75187ce70ecd5bf9499b45abb26a1b800f8a103e
JulianaZani <5i8kg7sf@students.codeinstitute.net>
2025-02-19 08:08:35 +0000

    create javascript file and add informations in html and js

---
+++ script.js
@@ -0,0 +1,14 @@
+const questions = [
+    { 
+        question: "Do you prefer individual or group training?", 
+        options: ["Individual", "In group"]
+    },
+    { 
+        question: "What is your main objective?", 
+        options: ["Lose weight", "Gain mass", "Endurance", "Relaxation"]
+    },
+    { 
+        question: "What type of activity do you like most?", 
+        options: ["HIIT", "Bodybuilding", "Running", "Yoga"]
+    }
+];
